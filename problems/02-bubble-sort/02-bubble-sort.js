20fd10c377b020a3419a1a44977ed1b4ce71855f
ceciliapichardo <cecmaria23@gmail.com>
2020-06-16 12:08:21 -0700

    finished bubblesort problem

--- problems/02-bubble-sort/02-bubble-sort.js
+++ problems/02-bubble-sort/02-bubble-sort.js
@@ -1,7 +1,30 @@
-/* 2. Bubble Sort
+/* Author: Cecilia Pichardo
+ * Date: June 16, 2020
+ * Bubble Sort
+ *      - Repeatedly swaps adjacent elements if they are in wrong order
 Define function: bubbleSort(numArray)
 Use the bubble sort algorithm to sort the array.
 Return the sorted array. */
+
+//Sample array
+let array = [8, 7, 6, 5, 4, 10]
+
+//Function swaps adjacent numbers until array is in order
 function bubbleSort(numArray) {
-  
+    n = numArray.length
+    for(let i = 0; i < n-1; i++) {
+        for(let j = 0; j < n-i-1; j++) {
+            if(numArray[j] > numArray[j+1]) { 
+                //swaps second element with the current
+                let temp = numArray[j]
+                numArray[j] = numArray[j+1]
+                numArray[j+1] = temp
+            }
+        }
+    }
+    return numArray
 }
+
+//Output to console
+console.log(`Before: ${array}`);
+console.log(`After: ${bubbleSort(array)}`);
