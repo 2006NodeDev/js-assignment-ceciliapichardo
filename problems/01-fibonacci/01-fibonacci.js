7944407d79c6fc41b64dbfdb598c1216efb983bb
ceciliapichardo <cecmaria23@gmail.com>
2020-06-16 11:36:57 -0700

    finished fibonacci problem

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -1,6 +1,27 @@
-/* 1. Fibonacci
-Define function: fib(n) 
-Return the nth number in the fibonacci sequence. */
-function fib(n) {
+/* Author: Cecilia Pichardo
+ * Date: June 16, 2020
+ * Fibonacci
+ *    - The next number is found by adding up the two numbers before
+ *    - Sequence: n=1 n=2 n=3 n=4 n=5 ...
+ *                 0   1   1   2   3 ...
+ */
 
+//Dictionary
+let nums = {
+    1:0,
+    2:1
 }
+
+//Function returns nth number in fibonacci sequence
+let fib = (n) => {
+    if(nums[n] || nums[n] === 0) {
+        return nums[n]
+    }
+    else {
+        nums[n] = fib(n - 1) + fib(n - 2)
+        return nums[n]
+    }
+}
+
+//Output to console
+console.log(`${fib(1)}, ${fib(2)}, ${fib(3)}, ${fib(4)}, ${fib(5)}...${fib(10)}`);
