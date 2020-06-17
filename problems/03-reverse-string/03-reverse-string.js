6d96d98696c96e84ca188cfb8d0fa18ba3850de0
ceciliapichardo <cecmaria23@gmail.com>
2020-06-17 13:25:13 -0700

    problem 3

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -1,6 +1,21 @@
-/* 3. Reverse String
-Define function: reverseStr(someStr)
-Reverse and return the String. */
+/* Author: Cecilia Pichardo
+ * Date: June 17, 2020
+ * Reverse String
+ *      - Takes in string as parameter
+ *      - Uses split() to split string into an array
+ *      - Uses reverse() to reverse the array
+ *      - Uses join() to join elements of array back into a string
+ *      - Returns reversed string
+ */
+
+//Function inputs a string and returns reversed string
 function reverseStr(someStr) {
-  
+    /*var split = someStr.split("");
+    var reverse = split.reverse();
+    var join = reverse.join("");
+    return join */
+    return someStr.split("").reverse().join("");
 }
+
+//Output to console
+console.log(reverseStr("hello world"));
